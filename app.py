6bf9f13dc1807f51d5ab9efb0a70e95eb2542589
GitBurtHET <43378078+GitBurtHET@users.noreply.github.com>
2020-12-23 23:28:16 +0100

    greetings

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def generate_buzz():
     page = '<html><body><h1>'
-    page += 'test<br><br>'
+    page += 'Greetings from Bart B & Thijs Z</br>'
     page += generator.generate_buzz()
     page += '</h1></body></html>'
     return page
